7a189bf2ee42dd42ff26ae30d8b26c2cc6a89813
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 20:31:35 +0000

    project setup and first component

---
+++ design-systems/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import logo from './logo.svg';
+import './App.css';
+
+const Heading = styled.h1`
+  font-size: 2rem;
+  color: greenyellow;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <Heading>
+            Edit <code>src/App.js</code> and save to reload.
+          </Heading>
+          <a
+            className="App-link"
+            href="https://reactjs.org" 
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
