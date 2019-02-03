fcd36669091891c0a4e5f72bf50b8a87e8e113cc
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-02-03 23:21:02 +0000

    scoping the componentes

--- design-systems/src/components/layouts/Header.js
+++ design-systems/src/components/layouts/Header.js
@@ -2,21 +2,17 @@ import React from 'react';
 import logo from '../../logo.svg';
 import styled from 'styled-components';
 
-const Header = () => {
-  return (
-    <AppHeader>
+const Header = ({ className }) => (
+    <header className={className}>
         <img src={logo} alt="logo" className="logo" />
-    </AppHeader>
-  );
-};
+    </header>
+);
 
-const AppHeader = styled.header`
+export default styled(Header)`
     background: #524763;
     padding: 10px 5%;
-    
+
     .logo {
         width: 60px;
     }
 `;
-
-export default Header;
